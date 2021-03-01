19c30bdd644b9f765826aba5361fe29328ada47f
Paige Kelly <pckelly.co@gmail.com>
2021-03-01 13:15:09 -0700

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement('h2');
+h2.textContent = "This is content was added by Javascript";
+document.querySelector('body').appendChild(h2);
+
+const p = document.createElement('p');
+p.textContent = "More content added as a paragraph by Javascript";
+document.querySelector('body').appendChild(p);
